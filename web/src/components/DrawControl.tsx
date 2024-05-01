38c9ea3c6f51e0d5bbfa0da9a96ccd344b8ddd6d
Ollie Walsh <ollie.walsh@gmail.com>
2024-05-01 16:35:51 +0100

    Fix mapboxdraw classes for maplibre

--- web/src/components/DrawControl.tsx
+++ web/src/components/DrawControl.tsx
@@ -13,6 +13,13 @@ type DrawControlProps = ConstructorParameters<typeof MapboxDraw>[0] & {
     onDelete: (evt: any) => void;
 };
 
+type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+
+const mbClasses = MapboxDraw.constants.classes as Writeable<any>;
+mbClasses.CONTROL_BASE  = 'maplibregl-ctrl';
+mbClasses.CONTROL_PREFIX = 'maplibregl-ctrl-';
+mbClasses.CONTROL_GROUP = 'maplibregl-ctrl-group';
+
 export default function DrawControl(props: DrawControlProps) {
     const mp = useControl<MapboxDraw>(
         () => new MapboxDraw(props),
